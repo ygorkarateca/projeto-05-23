3051023beebdbd88ac0b4288226f5b84d17aecd8
Ygor Fernandes <ygorkarateca@gmail.com>
2023-05-29 18:02:32 -0300

    Por hoje so

---
+++ aula7.py
@@ -0,0 +1,19 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Ygor Fernandes'
+# soma_dois_numeros = 2023 - 1993
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(f'Nome:{nome_completo}, Data de Nascimento:{soma_dois_numeros}')
+
+nome = 'Ygor'
+idade = 29
+idade_e_maior_q_30 = idade >= 30
+
+print('Nome:', nome, 'Idade:', idade)
+print('Tem maior que 30 anos?', idade_e_maior_q_30)
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Ygor'
+sobrenome = 'Fernandes da Silva'
+idade = 29
+ano_nascimento = 1993 - idade
+maior_de_idade = idade >= 18
+altura_metros = float(1.80)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros, end='m')
