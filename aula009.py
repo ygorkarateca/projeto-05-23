5fb1e80fafbf4be5706ec8866fa13b4d388b9e7a
Ygor Fernandes <ygorkarateca@gmail.com>
2023-05-31 16:49:21 -0300

    Modificiando

---
+++ aula009.py
@@ -0,0 +1,24 @@
+adicao = 10 + 10
+print('Adição =', adicao)
+
+subtracao = 10 - 5
+print('Subtração =', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação =', multiplicacao)
+
+divisao = 10 / 2.2 # float
+print('Divisão =', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão Inteira =', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação =', exponenciacao)
+
+modulo = 25 % 5 # resto da divisão
+print('Módulo =', modulo)
+
+# Códigos acima serve para testar se é ÍMPAR ou PAR
+print(10 % 8 == 0) # Testagem se o número realmente é divisivel 
+print(10 % 2) # Testagem se número é IMPAR ou PAR
---
+++ doideiras/impa_ou_par.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um valor:'))
+
+if num % 2 == 0:
+    print('Número é PAR')
+else:
+    print('Número é IMPAR')
+    
