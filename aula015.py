9c2fddbc2cd2e3b92b359493a18d25201464cb91
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2023-06-03 08:05:38 -0300

    Create aula015.py

---
+++ aula015.py
@@ -0,0 +1,10 @@
+# nome = input('Qual seu nome? ')
+# print(f'O seu nome é {nome}')
+
+num1 = input('Digite um número:')
+num2 = input('digite outro número: ')
+
+int_num1 = int(num1)
+int_num2 = int(num2)
+
+print(f'A soma dos números é: {int_num1 + int_num2}')
