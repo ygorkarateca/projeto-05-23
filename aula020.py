c8d3f484752dc16dadadff531732b1e84f3f95bb
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2023-06-03 17:52:53 -0300

    Create aula020.py
    exercícios da aula020

---
+++ aula020.py
@@ -0,0 +1,9 @@
+num1 = float(input('Digite um valor: '))
+num2 = float(input('Digite outro valor: '))
+
+if num1 > num2:
+  print(f'{num1=} é maior que o {num2=}')
+elif num2 > num1:
+  print(f'{num2=} é maior que o {num1=}')
+else:
+  print("OS NÚMEROS SÃO IGUAIS!!")
