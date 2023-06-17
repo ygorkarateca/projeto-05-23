c8aad883c0d4900006002d4a2ff4945b4dbdc7e4
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2023-06-17 12:59:43 -0300

    Create aula027.py

---
+++ aula027.py
@@ -0,0 +1,15 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá Mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres de str
+"""
+
+variavel = 'Olá Mundo'
+print(len(variavel))
+print(variavel[::-1])
+print(variavel[-4])
+print(variavel[4:])
