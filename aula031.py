aa6313abc960751c1bd20ed55d074821ebf44161
Ygor Fernandes <ygorkarateca@gmail.com>
2023-06-22 19:12:35 -0300

    Today

--- aula031.py
+++ aula031.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 Flag (Bandeira) - Marcar um local
 None = Não valor
@@ -6,28 +5,22 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
 
-v1 = 'a'
-v2 = 'b'
-print(id(v1))
-print(id(v2))
-
-# condicao = False
-
-# if condicao:
-#     print('Faça algo')
-# else:
-#     print('Não faça algo')
-=======
-"""
-Flag(Bandeira) - Marcar um local
-None = Não valor
-is e is not = é ou não é(tipo, valor, identidade)
-id = Identidade
-"""
+# v1 = 'a'
+# v2 = 'b'
+# print(id(v1))
+# print(id(v2))
 
 condicao = False
+passou_no_if = None
 
 if condicao:
+    passou_no_if = True
     print('Faça algo')
 else:
     print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
