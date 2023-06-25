2ad7d92f877cf93470a94466431c27a3d1850a7d
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2023-06-25 14:28:31 -0300

    EXCE05.py

--- aula032.py
+++ aula032.py
@@ -1,15 +1,25 @@
+from time import sleep
+
 """
 Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-# num = int(input('Digite um número:'))
 
-# if num == int:
-#     print('Não é inteiro')
-# else:
-#     print('Número não é inteiro')
+num = input('Digite um número: ')
 
+
+if not num.isdigit():
+  print('Número não é inteiro')
+else:
+  num = int(num)
+  
+  if num % 2 == 0:
+    print('Número é PAR')
+  else:
+    print('Número é IMPAR')
+sleep(2)
+print()
 
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
@@ -17,16 +27,17 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
-nome = str(input('Digite seu nome:'))
-hora = int(input('Que horas são?'))
+nome = str(input('Digite seu nome: '))
+hora = int(input('Que horas são? '))
 
-if hora < 0:
-    print('Bom dia', nome)
-    if hora >= 12:
-        print('Boa tarde', nome)
+if hora >= 0 and hora < 11:
+  print(f'Bom dia, {nome}')
+elif hora >= 12 and hora < 17:
+  print(f'Boa tarde, {nome}')
 else:
-    print('Boa noite', nome)
-
+  print(f'Boa noite, {nome}')
+sleep(1)
+print()
 
 
 """
@@ -34,3 +45,16 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+
+nome = input('Digite o seu primeiro nome: ')
+
+if len(nome) <= 4:
+  print('Seu nome é curto')
+elif len(nome) <=6:
+  print('Seu nome é normal')
+else:
+  print('Seu nome é muito grande!')
+  
+print()
+sleep(1)
+print('ACABEI SEU ARROMBADO!')
