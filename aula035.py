5dc57dda92f377aca2bc3264984f99fcd236de10
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2023-06-27 14:21:49 -0300

    CONTADOR DE NUMEROS.py

---
+++ aula035.py
@@ -0,0 +1,12 @@
+"""
+Repetições
+while (enquanto)
+Executra uma ação enquanto uma condição for verdade
+Loop infinito -> Quando um código não tem fim
+"""
+
+contador = 0
+while contador < 10:
+  contador = contador + 1
+  print(contador)
+print('Acabou')
